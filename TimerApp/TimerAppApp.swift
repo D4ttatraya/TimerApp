f6e8922cdac43b5d754eb7e39fae04605cf07ba8
d4ttatraya <d4ttatraya@gmail.com>
2022-11-16 13:33:50 +0530

    added new project

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by D4ttatraya on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
